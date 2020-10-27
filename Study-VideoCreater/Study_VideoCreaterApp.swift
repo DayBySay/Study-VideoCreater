9a0e14ad5735c11d1ba95b43478f550d137bae67
daybysay <yukita.sekai+github@gmail.com>
2020-10-28 08:59:50 +0900

    initial commit

---
+++ Study-VideoCreater/Study_VideoCreaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_VideoCreaterApp.swift
+//  Study-VideoCreater
+//
+//  Created by Takayuki Sei on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct Study_VideoCreaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
