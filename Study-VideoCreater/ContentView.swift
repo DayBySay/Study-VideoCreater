887e353e94f56bde38bb9c898b8b84c31b73d4db
daybysay <yukita.sekai+github@gmail.com>
2020-10-28 09:49:09 +0900

    List実装

--- Study-VideoCreater/ContentView.swift
+++ Study-VideoCreater/ContentView.swift
@@ -7,11 +7,47 @@
 
 import SwiftUI
 
+let items = [
+    DetailViewItem(name: "hoge"),
+    DetailViewItem(name: "fuga"),
+    DetailViewItem(name: "nyossu"),
+]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ListView()
+        }
     }
+}
+
+struct ListView: View {
+    var body: some View {
+        List(items) { item in
+            NavigationLink(
+                destination: DetailView(item: item),
+                label: {
+                    Text(item.name)
+                })
+        }
+        .navigationBarTitle("Create Media")
+    }
+}
+
+struct DetailView: View {
+    var item: DetailViewItem
+
+    var body: some View {
+        VStack {
+            Text(item.name)
+        }
+        .navigationBarTitle(item.name)
+    }
+}
+
+struct DetailViewItem: Equatable, Identifiable {
+    var id: String { name }
+    let name: String
 }
 
 struct ContentView_Previews: PreviewProvider {
