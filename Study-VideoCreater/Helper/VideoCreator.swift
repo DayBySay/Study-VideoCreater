3e0b61a17357025ce2d8cfd9e9ea98676790a770
daybysay <yukita.sekai+github@gmail.com>
2020-10-30 00:53:27 +0900

    ...

--- Study-VideoCreater/Helper/VideoCreator.swift
+++ Study-VideoCreater/Helper/VideoCreator.swift
@@ -10,39 +10,19 @@ import AVFoundation
 
 struct VideoCreator {
     private var url: URL { URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("\(UUID().uuidString).mov") }
-    private let width = 320
-    private let height = 320
-    private var rect: CGRect { CGRect(origin: .zero, size: CGSize(width: width, height: height)) }
-    private var outputSettings: [String: Any] {
-        [
-            AVVideoCodecKey: AVVideoCodecType.h264,
-            AVVideoWidthKey: width,
-            AVVideoHeightKey: height,
-        ]
-    }
-    private var sourcePixelBufferAttributes: [String: Any] {
-        [
-            kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB),
-            kCVPixelBufferWidthKey as String: width,
-            kCVPixelBufferHeightKey as String: height,
-        ]
-    }
-    private let fps = 30
-    private var duration: CMTime {
-        CMTime(seconds: Double(10), preferredTimescale: CMTimeScale(fps))
-    }
 
     func createSimpleVideo() -> URL? {
-        let image = createColorImage(color: .red)!
-        return createVideoFromImage(image: image)
+        let configuration = VideoConfiguration.makeDefault()
+        let image = createColorImage(color: .red, rect: configuration.rect)!
+        return createVideoFromImage(configuration: configuration, image: image)
     }
     
     func createVideoFromImage() -> URL? {
         let image = UIImage(named: "Image")!
-        return createVideoFromImage(image: image)
+        return createVideoFromImage(configuration: VideoConfiguration.makeDefault(), image: image)
     }
     
-    private func createColorImage(color: UIColor) -> UIImage? {
+    private func createColorImage(color: UIColor, rect: CGRect) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
         color.setFill()
         UIRectFill(rect)
@@ -51,26 +31,26 @@ struct VideoCreator {
         return image
     }
     
-    private func createVideoFromImage(image: UIImage) -> URL? {
+    private func createVideoFromImage(configuration: VideoConfiguration, image: UIImage) -> URL? {
         let url = self.url
         guard let writer = try? AVAssetWriter(outputURL: url, fileType: .mov) else {
-            print("URLがダメ")
             return nil
         }
-
         
-        let input = AVAssetWriterInput(mediaType: .video, outputSettings: outputSettings)
+        // VideoTrack追加用のInputを作成しWriterに追加
+        let input = AVAssetWriterInput(mediaType: .video, outputSettings: configuration.outputSettings)
         writer.add(input)
         
+        // CVPixelBufferから書き込むのでAdaptorを作成
         let adaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: input,
-                                                         sourcePixelBufferAttributes: sourcePixelBufferAttributes)
-        input.expectsMediaDataInRealTime = true
+                                                           sourcePixelBufferAttributes: configuration.sourcePixelBufferAttributes)
         
+        // videoに書き込むためのバッファを作成
         let ciImage = CIImage(image: image)!
         var pixelBuffer: CVPixelBuffer? = nil
         CVPixelBufferCreate(kCFAllocatorDefault,
-                            width,
-                            height,
+                            Int(configuration.rect.width),
+                            Int(configuration.rect.height),
                             kCVPixelFormatType_32ARGB,
                             [
                                 kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
@@ -79,26 +59,52 @@ struct VideoCreator {
                             &pixelBuffer)
         CIContext().render(ciImage, to: pixelBuffer!)
         
-        guard writer.startWriting() else {
-            print("書けないヨ")
-            return nil
-        }
-        writer.startSession(atSourceTime: CMTime(seconds: 0, preferredTimescale: CMTimeScale(fps)))
-        if !adaptor.append(pixelBuffer!, withPresentationTime: CMTime(seconds: 0, preferredTimescale: CMTimeScale(fps))) {
-            print("ついかしっぱい")
-        }
-        if !adaptor.append(pixelBuffer!, withPresentationTime: duration) {
-            print("ついかしっぱい")
-        }
-        
+        // 書き込み可能かどうかの確認
+        guard writer.startWriting() else { return nil }
+        writer.startSession(atSourceTime: CMTime(seconds: 0, preferredTimescale: CMTimeScale(configuration.fps)))
+
+        adaptor.append(pixelBuffer!, withPresentationTime: CMTime(seconds: 0, preferredTimescale: CMTimeScale(configuration.fps)))
+        adaptor.append(pixelBuffer!, withPresentationTime: configuration.duration)
+
         input.markAsFinished()
-        writer.endSession(atSourceTime: duration)
+        writer.endSession(atSourceTime: configuration.duration)
         writer.finishWriting {
-            print("ビデオができた")
+            print("ビデオ完成")
         }
         
         print("open \(NSTemporaryDirectory())")
         
         return url
+    }
+}
+
+struct VideoConfiguration {
+    var rect: CGRect
+    var outputSettings: [String: Any]
+    var sourcePixelBufferAttributes: [String: Any]
+    var fps: Int
+    var duration: CMTime
+    
+    static func makeDefault() -> VideoConfiguration {
+        let width = 320
+        let height = 320
+        let rect = CGRect(origin: .zero, size: CGSize(width: width, height: height))
+        let outputSettings: [String: Any] =
+            [
+                AVVideoCodecKey: AVVideoCodecType.h264,
+                AVVideoWidthKey: width,
+                AVVideoHeightKey: height,
+            ]
+        
+        let sourcePixelBufferAttributes: [String: Any] =
+            [
+                kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB),
+                kCVPixelBufferWidthKey as String: width,
+                kCVPixelBufferHeightKey as String: height,
+            ]
+        let fps = 30
+        let duration: CMTime = CMTime(seconds: Double(10), preferredTimescale: CMTimeScale(fps))
+        
+        return VideoConfiguration(rect: rect, outputSettings: outputSettings, sourcePixelBufferAttributes: sourcePixelBufferAttributes, fps: fps, duration: duration)
     }
 }
